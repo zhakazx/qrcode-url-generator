b52142454a92644c0fc44f7936a44452bcf1ff25
Zhaka Hidayat <zhakazx@gmail.com>
2024-09-09 19:32:10 +0800

    feat: add footer component

---
+++ components/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://github.com/zhakazx/qrcode-url-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Made by Zhaka Hidayat
+        </a>
+      </footer>
+  )
+}
