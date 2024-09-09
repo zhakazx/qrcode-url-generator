a3a13c1ab5d92748baf0e64065350852de28107e
Zhaka Hidayat <zhakazx@gmail.com>
2024-09-09 21:39:41 +0800

    feat(ui): add select component

--- components/main.tsx
+++ components/main.tsx
@@ -3,11 +3,17 @@ import { Input } from "./ui/input";
 
 export default function Main() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 sm:items-start">
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          
+    <main className="flex flex-col md:flex-row gap-8 row-start-2 w-full md:justify-center items-center sm:items-start">
+      <div className="grid w-full max-w-sm items-center gap-1.5">
+        <div className="flex flex-row gap-2">
+          <Input 
+            type="text"
+            id="url" 
+            placeholder="Enter URL ..." 
+          />
+          <Button>Generate</Button>
         </div>
-
+      </div>
     </main>
   );
 }
