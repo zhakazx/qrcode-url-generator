2d76389f65ac8d1e8e9acc87a1034787ece7ba42
Zhaka Hidayat <zhakazx@gmail.com>
2024-10-17 07:08:09 +0800

    feat: adding background pattern

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="row-start-1 flex justify-center gap-3">
-      <h1 className="text-2xl font-bold uppercase text-center">
+      <h1 className="text-4xl font-bold uppercase text-center">
         QR Code URL Generator 
         <span className="max-sm:hidden">✨</span>
       </h1>
